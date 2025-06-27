5080664fefe4e32358156a79815a6c79eb83d8d6
carl <carldjapardi7@gmail.com>
2025-06-27 01:13:05 -0700

    test if commit works

--- app/src/main/java/com/example/sensortracking/ui/screens/track/TrackScreen.kt
+++ app/src/main/java/com/example/sensortracking/ui/screens/track/TrackScreen.kt
@@ -185,7 +185,7 @@ fun TrackScreen(
         )
     }
 
-    // Clamp zoom between 0% and 300%
+    // Zoom between 0% and 300%
     val minZoom = 0.0f
     val maxZoom = 3.0f
     val zoomPercent = (uiState.zoom * 100).roundToInt().coerceIn(0, 300)
